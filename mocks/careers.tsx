ffb087e4b1cec454eeb3e94e9514359b00b11c66
David John <david.john.23120@gmail.com>
2024-08-21 13:55:38 +0700

    update 2

--- mocks/careers.tsx
+++ mocks/careers.tsx
@@ -2,41 +2,41 @@ export const careers = [
     {
         title: "Visual Designer",
         content:
-            "Stay current on the latest Onix project developments, news, and content, updated daily.",
+            "Visual Designer is a professional who specializes in creating visually engaging and effective designs for various media. Their role typically involves using design principles and tools to enhance user experiences, communicate messages, and solve design problems across different platforms.",
         image: "/images/blog/photo-1.png",
         status: ["REMOTE", "FULL-TIME"],
-        url: "https://ui8.net/",
+        url: "https://dddc.io/",
     },
     {
         title: "Web3 Developer",
         content:
-            "Stay current on the latest Onix project developments, news, and content, updated daily.",
+            "Web3 Developer is a software engineer who specializes in building decentralized applications (dApps) and platforms that leverage blockchain technology and the principles of Web3. Web3 represents the next generation of the internet, characterized by decentralized, peer-to-peer interactions and a focus on user ownership and control.",
         image: "/images/blog/photo-2.png",
         status: ["REMOTE", "CONTRACT"],
-        url: "https://ui8.net/",
+        url: "https://dddc.io/",
     },
     {
         title: "Project Manager",
         content:
-            "Stay current on the latest Onix project developments, news, and content, updated daily.",
+            "Project Manager is a professional responsible for planning, executing, and closing projects effectively and efficiently. Their role is crucial in ensuring that a project meets its goals, stays within scope, budget, and timeline, and delivers the expected results.",
         image: "/images/blog/photo-3.png",
         status: ["REMOTE", "FULL-TIME"],
-        url: "https://ui8.net/",
+        url: "https://dddc.io/",
     },
     {
         title: "3D Illustrator",
         content:
-            "Stay current on the latest Onix project developments, news, and content, updated daily.",
+            "3D Illustrator is a creative professional who specializes in creating three-dimensional visual content using digital tools and techniques. Their work involves producing 3D graphics and illustrations that can be used in various fields, including entertainment, advertising, product design, and more.",
         image: "/images/blog/photo-4.png",
         status: ["REMOTE", "CONTRACT"],
-        url: "https://ui8.net/",
+        url: "https://dddc.io/",
     },
     {
         title: "Frontend Developer",
         content:
-            "Stay current on the latest Onix project developments, news, and content, updated daily.",
+            "Frontend Developer is a software developer who specializes in creating and implementing the visual and interactive aspects of a website or web application that users interact with directly. They work on the client side of web development, ensuring that the user interface (UI) is both functional and visually appealing.",
         image: "/images/blog/photo-1.png",
         status: ["REMOTE", "FULL-TIME"],
-        url: "https://ui8.net/",
+        url: "https://dddc.io/",
     },
 ];
