82d12cf8f8e06f3a2dc0b2f6ea4b9fd61d1652e1
David John <david.john.23120@gmail.com>
2024-08-21 13:46:44 +0700

    update

--- constants/faqs.tsx
+++ constants/faqs.tsx
@@ -1,17 +1,17 @@
 export const faqs = [
-    {
-        title: "Who can join the project?",
-        content:
-            "Your data can be transformed, organized, and shared across applications for anyone to query with just a few keystrokes. Your data can be transformed, organized, and shared across applications for anyone to query with just a few keystrokes. Stay current on the latest DDDC project developments, news, and content, updated daily. Stay current on the latest DDDC project developments, news, and content, updated daily.",
-    },
-    {
-        title: "What is On-point service",
-        content:
-            "Your data can be transformed, organized, and shared across applications for anyone to query with just a few keystrokes. Your data can be transformed, organized, and shared across applications for anyone to query with just a few keystrokes. Stay current on the latest DDDC project developments, news, and content, updated daily. Stay current on the latest DDDC project developments, news, and content, updated daily.",
-    },
-    {
-        title: "What is UI8",
-        content:
-            "Your data can be transformed, organized, and shared across applications for anyone to query with just a few keystrokes. Your data can be transformed, organized, and shared across applications for anyone to query with just a few keystrokes. Stay current on the latest DDDC project developments, news, and content, updated daily. Stay current on the latest DDDC project developments, news, and content, updated daily.",
-    },
+  {
+    title: "Who can join the project?",
+    content:
+      "The eligibility to join a project depends on various factors, including the nature of the project, its goals, and the criteria set by its organizers. Here’s a general outline of who might be eligible to join a project",
+  },
+  {
+    title: "What is On-point service",
+    content:
+      "On-point service generally refers to a high standard of service that is precise, effective, and aligns perfectly with the needs and expectations of the customer or client. Here’s a breakdown of what this term usually implies",
+  },
+  {
+    title: "What is DDDC?",
+    content:
+      "DDDC Coin refers to a specific cryptocurrency associated with the Decentralized Data Distribution Chain (DDDC) project. The coin is part of a blockchain-based platform aimed at addressing issues related to data distribution and management.",
+  },
 ];
