8a679bc09e8eb4c704e5d8647934b008873c38f0
David John <david.john.23120@gmail.com>
2024-08-21 12:59:37 +0700

    v0.1.0

---
+++ constants/socials.tsx
@@ -0,0 +1,22 @@
+export const socials = [
+    {
+        icon: "discord",
+        href: "https://dddc.io/",
+    },
+    {
+        icon: "twitter",
+        href: "https://dddc.io/",
+    },
+    {
+        icon: "instagram",
+        href: "https://dddc.io/",
+    },
+    {
+        icon: "telegram",
+        href: "https://dddc.io/",
+    },
+    {
+        icon: "facebook",
+        href: "https://dddc.io/",
+    },
+];
