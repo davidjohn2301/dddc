e4eee294de0b1ba6599b91cdcdf15a65f665eb28
David John <david.john.23120@gmail.com>
2024-08-26 13:29:36 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ParallaxProvider } from "react-scroll-parallax";
 import "../styles/app.sass";
 import "../i18n/i18n"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
